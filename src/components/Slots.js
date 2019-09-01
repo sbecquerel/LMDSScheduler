fd766242fee11959ee0e000a0fdbdaacbf72d6ad
Sébastien Becquerel <s.becquerel@epiconcept.fr>
2019-09-01 10:08:25 +0200

    Change style

--- src/components/Slots.js
+++ src/components/Slots.js
@@ -2,11 +2,13 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
 import classNames from 'classnames'
+import moment from 'moment'
 
 const Slots = ({match, calendar}) => {
   const 
     teacherName = match.params.teacher_name,
-    teacher = calendar.find(teacher => teacher.name === teacherName)
+    teacher = calendar.find(teacher => teacher.name === teacherName),
+    currentDay = moment().format('dddd')
 
   if (teacher === undefined) {
     return <div>Undefined calendar for teacher {teacherName}</div>
@@ -17,10 +19,15 @@ const Slots = ({match, calendar}) => {
       <h1 className="text-center">{match.params.teacher_name}</h1>
       <div className="list-group">
           {teacher.slots.map((slot, i) => (
-          <Link key={i} to={`/students/${teacherName}/${i}`} className={classNames("list-group-item", "list-group-item-action")}>
+          <Link key={i} to={`/students/${teacherName}/${i}`} className={classNames(
+            "list-group-item", 
+            "list-group-item-action",
+            (currentDay === slot.day ? "font-weight-bold" : "text-muted")
+            )}>
               {slot.day.charAt(0).toUpperCase()}{slot.day.slice(1)} {slot.hour}h{slot.minutes > 0 ? slot.minutes : ''}
           </Link>
-          ))}
+          )
+          )}
       </div>
     </div>
   )
